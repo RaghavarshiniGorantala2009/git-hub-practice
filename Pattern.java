ad9fc967e9fea24bd91ea6b21a6dc3cc3f5049f4
RaghavarshiniGorantala2009 <nagarajugorantala09@gmail.com>
2026-02-16 18:30:52 +0530

    inverse pattern

--- Pattern.java
+++ Pattern.java
@@ -1,7 +1,7 @@
 public class Pattern{
     public static void main(String[] args) {
-        int n=5;
-    for(int i=0;i<n;i++){
+        int n=10;
+    for(int i=10;i>0;i--){
         for(int j=0;j<n-i;j++){
             System.out.print("*");
         }
