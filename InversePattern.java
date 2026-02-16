552a5042b972932d80ed0c3757953d99c5f1950c
RaghavarshiniGorantala2009 <nagarajugorantala09@gmail.com>
2026-02-16 19:09:07 +0530

    inverse pattern printing

---
+++ InversePattern.java
@@ -0,0 +1,11 @@
+public class InversePattern {
+  public static void main(String[] args) {
+    int n=10;
+    for(int i=10;i>0;i--){
+        for(int j=0;j<n-i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+  }  
+}
