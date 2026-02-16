6aeade75d8e925919c123e257aa2dccf9e5d9285
RaghavarshiniGorantala2009 <nagarajugorantala09@gmail.com>
2026-02-16 18:52:13 +0530

    Pattern printing

--- Pattern.java
+++ Pattern.java
@@ -1,7 +1,7 @@
-public class Pattern{
-    public static void main(String[] args) {
+public class Pattern {
+ public static void main(String[] args) {
         int n=10;
-    for(int i=10;i>0;i--){
+    for(int i=0;i<n;i++){
         for(int j=0;j<n-i;j++){
             System.out.print("*");
         }
@@ -9,3 +9,4 @@ public class Pattern{
     }
     }
 }
+ 
